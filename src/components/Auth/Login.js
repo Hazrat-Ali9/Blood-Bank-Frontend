435368f6d533caa1efba65fefa1beb4f0c403039
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-01-06 19:39:17 +0600

    login js 🚞 ☂

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { login } from "../../api/api";
 import { saveToken } from "../../utils/auth";
 import { Link } from "react-router-dom";
-
+// Login
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
